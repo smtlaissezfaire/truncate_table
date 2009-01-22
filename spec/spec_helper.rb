621d895f6e497b011cf5792c70634e42624eab9d
Scott Taylor <scott@railsnewbie.com>
2009-01-22 13:36:37 -0500

    Add support for loading db in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
 
 require "rubygems"
 require "spec"
+
+require "active_record"
+require File.dirname(__FILE__) + "/../init"
+
+db_file = File.dirname(__FILE__) + "/connection.yml"
+ActiveRecord::Base.establish_connection YAML.load(File.read(db_file))
+ActiveRecord::Migration.verbose = true
+
+ActiveRecord::Schema.define do  
+  create_table :users, :force => true do |t|
+    t.column  :username,  :string
+    t.timestamps
+  end
+end
